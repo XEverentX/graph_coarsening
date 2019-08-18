2dffb42d37fbec22ccd4ff163dd6b961b91a8de2
XEverentX <Inok.Alex@gmail.com>
2019-08-18 21:22:05 +0300

    Fixed tests for greedy

--- tests/source/greedy_test.cpp
+++ tests/source/greedy_test.cpp
@@ -9,7 +9,7 @@ TEST(greedy_test, personal_test_csr_1) {
     graph.edges = {1, 2, 3, 4, 4, 5};
     graph.weights = {20, 1, 1, 10, 5, 17};
     Matching expected;
-    Matching ans = LAM(graph);
+    Matching ans = greedy(graph);
     expected.edge_b = {0, 4, 2};
     expected.edge_e = {1, 5, 3};
     ASSERT_EQ(expected.edge_b, ans.edge_b);
@@ -23,7 +23,7 @@ TEST(greedy_test, personal_test_csr_2) {
     graph.edges = {1, 2, 3, 4, 4, 5};
     graph.weights = {30, 1, 2, 1, 20, 19};
     Matching expected;
-    Matching ans = LAM(graph);
+    Matching ans = greedy(graph);
     expected.edge_b = {0, 3};
     expected.edge_e = {1, 4};
     ASSERT_EQ(expected.edge_b, ans.edge_b);
@@ -37,7 +37,7 @@ TEST(greedy_test, personal_test_csr_3) {
     graph.edges = {1, 2, 3, 4, 4, 5};
     graph.weights = {30.4, 1, 2, 30.395, 30.39, 19};
     Matching expected;
-    Matching ans = LAM(graph);
+    Matching ans = greedy(graph);
     expected.edge_b = {0, 2};
     expected.edge_e = {1, 4};
     ASSERT_EQ(expected.edge_b, ans.edge_b);
@@ -45,13 +45,13 @@ TEST(greedy_test, personal_test_csr_3) {
 }
 
 TEST(greedy_test, personal_test_csr_4) {
-    CSR<double> graph;
+    CSR<int> graph;
     graph.n = 7;
     graph.offset = {0, 1, 2, 3, 4, 5, 6, 6};
     graph.edges = {4, 4, 4, 4, 5, 6};
     graph.weights = {20, 20, 15, 30, 40, 39};
     Matching expected;
-    Matching ans = LAM(graph);
+    Matching ans = greedy(graph);
     expected.edge_b = {4};
     expected.edge_e = {5};
     ASSERT_EQ(expected.edge_b, ans.edge_b);
